530c5e263e0e78f4747b30bfa85466b660752d7c
EduardoMorenoMarin <moondani182@gmail.com>
2025-11-16 00:34:35 -0500

    chore: update route after backend change

--- src/app/modules/purchases/services/purchases-api.routing.ts
+++ src/app/modules/purchases/services/purchases-api.routing.ts
@@ -1,5 +1,5 @@
 export const PURCHASES_API_ROUTES = {
     create: 'purchases/from-cart',
-    mySales: 'purchases/my-sales',
-    myPurchases: 'purchases/my-purchases',
+    mySales: 'sales',
+    myPurchases: 'purchases',
 }
